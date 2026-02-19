bef649bec4387aa7fb99b92fd1bb07377475c1a6
AGBowley <aguzinskibowley@gmail.com>
2026-02-19 02:57:51 +0000

    Refactor Cargo.toml and main.rs for Windows-specific dependencies; add GitHub and GitLab CI/CD workflows for automated releases

--- src-tauri/helper_executable/src/main.rs
+++ src-tauri/helper_executable/src/main.rs
@@ -1,19 +1,32 @@
+#[cfg(windows)]
 extern crate winapi;
 
-use std::env;
+#[cfg(windows)]
 use std::ffi::CString;
+#[cfg(windows)]
 use std::ptr;
+#[cfg(windows)]
 use winapi::um::shellapi::ShellExecuteA;
+#[cfg(windows)]
 use winapi::um::winuser::SW_SHOWNORMAL;
 
+#[cfg(not(windows))]
 fn main() {
+    eprintln!("helper_executable is only supported on Windows.");
+    std::process::exit(1);
+}
+
+#[cfg(windows)]
+fn main() {
+    use std::env;
+
     // Get the path to the game executable from the command-line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: helper_executable <path_to_game_executable>");
         return;
     }
-    
+
     let game_path: &String = &args[1];
     let game_path_cstr: CString = match CString::new(game_path.as_str()) {
         Ok(cstr) => cstr,
